4d676368e150a83601f96a2afe78c4813ebb098c
חנה שרון <323808220@mbydomain>
2021-11-02 19:57:57 +0200

    first step

---
+++ startup/src/main/java/com/example/springboot/textUtils.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+import java.util.Arrays;
+
+import org.apache.commons.lang3.StringUtils;
+public class textUtils {
+	private static String name="Chaya and Chana check if the string is empty";
+	 
+
+	public static String getText() {
+		if (StringUtils.isEmpty(name)) 
+			return "the text is empty";
+		return "the text is: " + name;
+	}
+
+}
